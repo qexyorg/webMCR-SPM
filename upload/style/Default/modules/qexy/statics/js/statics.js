bb7a4afd5e86bdf1f0c249b46a609332450784f8
Qexy <admin@qexy.org>
2015-08-09 17:42:16 +0300

    Remove unused functions

--- upload/style/Default/modules/qexy/statics/js/statics.js
+++ upload/style/Default/modules/qexy/statics/js/statics.js
@@ -1,14 +1,3 @@
-/**
- * Control moder panel (CMP) for webmcr
- * 
- * @author Qexy.org (admin@qexy.org)
- *
- * @copyright Copyright (c) 2014-2014 Qexy.org
- *
- * @version 1.0
- *
- */
-
 $(function(){
 	$("[rel='tooltip']").tooltip({container: 'body'});
 
@@ -16,18 +5,3 @@ $(function(){
 		return confirm("Вы уверены, что хотите удалить выбранный элемент?");
 	});
 });
-
-function bb(obj, leftcode, rightcode)
-{
-	if(document.selection)
-	{ // Для IE
-		var s = document.selection.createRange();
-		if (s.text){ s.text = leftcode + s.text + rightcode; }
-	}else{ // Opera, FireFox, Chrome
-
-		var start = obj.selectionStart;
-		var end = obj.selectionEnd;
-		s = obj.value.substr(start,end-start);
-		obj.value = obj.value.substr(0, start) + leftcode + s + rightcode + obj.value.substr(end);
-	}
-}
